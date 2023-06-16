4ccc34fa9f2a47c29439a4973da95717d9a93e3e
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-06-16 15:44:42 +0200

    modifiche di stile per le card

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,16 +1,20 @@
 
 @extends('layouts.app')
 @section('content')
-<div class="container justify-content-center">
-    <div class="card w-25">
-        <img src={{$product->image}} class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">{{$product->name}}</h5>
-          <p class="card-text">{{$product->description}}</p>
-          <p>{{$product->price}}</p>
-          <a href="{{ route('admin.products.index') }}" class="btn btn-primary">Torna indietro</a>
+<div class="container mb-5">
+    <div class="d-flex justify-content-center mt-5">
+        <div class="card w-25">
+            <img src={{$product->image}} class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{$product->name}}</h5>
+              <p class="card-text">{{$product->description}}</p>
+              <p>{{$product->price}}</p>
+              <a href="{{ route('admin.products.index') }}" class="btn btn-primary">Torna indietro</a>
+            </div>
         </div>
+
     </div>
+ 
 
 </div>
 
