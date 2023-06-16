5918aaffac8c5568d44f94c3f9e7980916365061
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-06-16 15:22:05 +0200

    Modifiche grafiche card

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,7 +1,24 @@
 
+@extends('layouts.app')
+@section('content')
+<div class="container justify-content-center">
+    <div class="card w-25">
+        <img src={{$product->image}} class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{$product->name}}</h5>
+          <p class="card-text">{{$product->description}}</p>
+          <p>{{$product->price}}</p>
+          <a href="{{ route('admin.products.index') }}" class="btn btn-primary">Torna indietro</a>
+        </div>
+    </div>
 
-<h1>{{$product->name}}</h1>
+</div>
 
-<a href="{{ route('admin.products.index') }}" class="btn btn-primary">torna a casa lessi</a>
-    
+
+
+  @endsection
+
+
+
+
 
