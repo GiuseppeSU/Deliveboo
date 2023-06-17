04a041cecb38a4144a51177e71d5d2fc3686312b
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-06-17 17:40:29 +0200

    Creazione piccolo layout per pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="jumbotron p-5 mb-4 bg-light rounded-3">
-    <div class="container py-5">
+<div class="jumbotron p-5 mb-4  rounded-3 bg-warning ">
+    <div class="container py-5 ">
         <div class="logo_laravel">
             <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-25">
                 <g clip-path="url(#clip0)" fill="#EF3B2D">
