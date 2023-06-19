66aaf7e282191c62e39de80b061c888533d56725
AngeloRoudani <ange.rouda@gmail.com>
2023-06-19 18:14:43 +0200

    modifiche

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -38,7 +38,7 @@ class UpdateProductRequest extends FormRequest
             'restaurant_id' => 'exists:restaurants,id',
             'name' => 'required|max:100',
             'slug' => ['required', 'max:110', Rule::unique('products')->ignore($this->product)],
-            'price' => 'required|numeric|decimal:0,2',
+            'price' => 'required|numeric|min:0|decimal:0,2',
             'description' => 'nullable',
             'image' => 'image|nullable|max:255',
             'visibility' => 'nullable',
@@ -55,6 +55,7 @@ class UpdateProductRequest extends FormRequest
             'price.required' =>'Il prezzo è richiesto',
             'price.decimal' => 'Impossibile inserire il prezzo richiesto',
             'price.numeric' => 'Il prezzo non può contenere lettere',
+            'price.min' => 'il prezzo non può essere negativo',
             'image.image' => 'Il formato del file non è un immagine'
         ];
     }
