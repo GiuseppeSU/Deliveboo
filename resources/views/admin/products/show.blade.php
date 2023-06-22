ae080e97a55ff414f7db310706ab90453d3b5e13
Valerio Salzano <vlr.slzn@gmail.com>
2023-06-22 09:46:57 +0200

    fix immagine che non si mostra

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -3,7 +3,7 @@
     <div class="container mb-5">
         <div class="d-flex justify-content-center mt-5">
             <div class="card w-25">
-                <img src={{ $product->image }} class="card-img-top" alt="...">
+                <img src={{ asset('storage/'.$product->image)}} class="card-img-top" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">{{ $product->name }}</h5>
                     <p class="card-text">{{ $product->description }}</p>
