d6f85d41c08e82664d2a88fe524a9126388d3784
Valerio Salzano <vlr.slzn@gmail.com>
2023-06-24 10:56:57 +0200

    modificata query

--- app/Http/Controllers/Api/RestaurantController.php
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -9,17 +9,18 @@ use App\Models\Product;
 
 class RestaurantController extends Controller
 {
-    public function index(Request $request) {
+    public function index(Request $request)
+    {
 
-        if($request->query('types')){
-            $types = explode(',',$request->query('types'));
+        if ($request->query('types')) {
+            $types = explode(',', $request->query('types'));
             $restaurants = Restaurant::with('types')
-                            ->join('restaurant_type', 'id', '=', 'restaurant_type.restaurant_id')->join('types', 'type_id', 'types.id')
-                            ->whereIn('types.slug',$types)
-                            ->select('restaurants.*')
-                            ->distinct()
-                            ->get();
-        }else{
+                ->join('restaurant_type', 'id', '=', 'restaurant_type.restaurant_id')->join('types', 'type_id', 'types.id')
+                ->whereIn('types.slug', $types)
+                ->select('restaurants.*')
+                ->distinct()
+                ->get();
+        } else {
             $restaurants = Restaurant::all();
         }
 
@@ -29,25 +30,19 @@ class RestaurantController extends Controller
         ]);
     }
 
-    public function show($id) {
+    public function show($slug)
+    {
+        $restaurant = Restaurant::where('restaurants.slug', $slug)
+            ->with('products')
+            ->join('products','restaurants.id','restaurant_id')
+            ->where('products.visibility', 1)
+            ->select('restaurants.*')
+            ->distinct()
+            ->get();
 
-        
-
-            $restaurant = Product::where('restaurant_id', '=', $id)
-                        ->where('visibility','=', 1)
-                        ->select('products.*')
-                        ->get();
-
-            return response()->json([
-                'success' => true,
-                'results' => $restaurant,
-            ]);
-        
-
-            abort(404);
-        
-
-    
+        return response()->json([
+            'success' => true,
+            'results' => $restaurant,
+        ]);
     }
-
 }
