135bbb072c744e0f4172b5caf7967968ba887b17
Emanuele Sacchetti <manusacco92@gmail.com>
2023-06-30 16:34:04 +0200

    aggiunta nuova card nella dashboard per ordini(ancora non implementata del tutto) e cambiata scritta dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,39 +6,64 @@
             {{ __('Dashboard') }}
         </h2>
         <div class="row justify-content-center">
-            <div class="col-sm-6 col-lg-3 col-md-6 mb-3">
+            <div class="col-lg-4 col-sm-12 col-md-12 mb-3">
                 @include('partials.sidebar')
             </div>
-            <div class="col-lg-9 col-sm-6 col-md-6">
+            <div class="col-lg-8 col-sm-12 col-md-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card rounded-4">
+                            <!--<div class="card-header">{{ __('User Dashboard') }}</div>-->
 
-                <div class="card rounded-4">
-                    <!--<div class="card-header">{{ __('User Dashboard') }}</div>-->
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+                            <div class="card-body">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
+                                <strong>
+                                    {{ __('Benvenuto nella tua area personale') }}
+                                </strong>
                             </div>
-                        @endif
-                        <strong>
-                            {{ __('Benvenuto nella tua area personale') }}
-                        </strong>
+                        </div>
+                    </div>
+
+                </div>
+
+
+                <br>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="card rounded-4">
+                            <h6 class="card-header text-center">
+                                <strong>
+                                    Numero di piatti
+                                </strong>
+                            </h6>
+                            <h1 class="text-center">
+                                <a href="{{ route('admin.products.index') }}" class="nav-link link-body-emphasis">
+                                    {{ $products->count() }}
+                                </a>
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="card rounded-4">
+                            <h6 class="card-header text-center">
+                                <strong>
+                                    Numero di piatti
+                                </strong>
+                            </h6>
+                            <h1 class="text-center">
+                                <a href="{{ route('admin.products.index') }}" class="nav-link link-body-emphasis">
+                                    {{ $products->count() }}
+                                </a>
+                            </h1>
+                        </div>
                     </div>
                 </div>
 
-                <br>
-                <div class="card w-25 rounded-4">
-                    <h6 class="card-header text-center">
-                        <strong>
-                            Numero di piatti
-                        </strong>
-                    </h6>
-                    <h1 class="text-center">
-                        <a href="{{ route('admin.products.index') }}" class="nav-link link-body-emphasis">
-                            {{ $products->count() }}
-                        </a>
-                    </h1>
-                </div>
+
             </div>
         </div>
     </div>
